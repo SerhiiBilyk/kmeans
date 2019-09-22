7cf2a5bbe256a80ffc1d92a82f60364d1ca7543d
Serhii <sergiybiluk@gmail.com>
2019-09-22 21:14:51 +0200

    Add new method

--- src/kmeans.rs
+++ src/kmeans.rs
@@ -1,5 +1,5 @@
 use crate::utils;
-use utils::{distance, random_number};
+use utils::{distance, random_number, mean};
 
 type Point = [f32; 2];
 
@@ -82,13 +82,12 @@ impl KlusterMeans {
 
     pub fn assign_point_to_centroid(&mut self, point_index: usize) -> bool {
         let last_assigned = self.centroid_exist(point_index);
-        let point = self.points[point_index];
         let mut min_distance = 0.0;
         let mut assigned_centroid: Option<i32> = None;
 
         for index in 0..self.centroids.len() {
             let centroid = self.centroids[index];
-            let distance_to_centroid = distance(point, centroid);
+            let distance_to_centroid = distance(self.points[point_index], centroid);
             if min_distance == 0.0 || distance_to_centroid < min_distance {
                 min_distance = distance_to_centroid;
                 assigned_centroid = Some(index as i32);
@@ -104,6 +103,37 @@ impl KlusterMeans {
         };
 
         false
+    }
+    pub fn assign_points_to_centroids(&mut self) -> bool {
+        let mut was_any_reassigned = false;
+        for index in 0..self.points.len() {
+            let was_reassigned = self.assign_point_to_centroid(index);
+            if was_reassigned {
+                was_any_reassigned = true;
+            }
+        }
+        was_any_reassigned
+    }
+    pub fn get_points_for_centroid(&self, centroid_index: usize) -> Vec<Point> {
+        let mut points: Vec<Point> = vec![];
+        for index in 0..self.points.len() {
+            let assignment = self.centroid_exist(index);
+            if let Some(value) = assignment {
+                if value == centroid_index as i32 {
+                    points.push(self.points[index])
+                }
+            }
+        }
+        points
+    }
+    pub fn update_centroid_location(&self, index:usize){
+        let centroid_points = self.get_points_for_centroid(index);
+        let dimensionality = self.get_dimensionality();
+        let centroid = vec![];
+        for dimension in 0..dimensionality {
+            let map = centroid_points.iter().map(|elem|elem[dimension])
+            centroid.push()
+        }
     }
     pub fn reset(&mut self) {
         self.centroid_assignments = vec![];
--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,10 @@
 
 use rand::{thread_rng, Rng};
 
+pub fn mean(numbers: Vec<f32>) -> f32 {
+    numbers.iter().fold(0.0, |acc, value| acc + value) / numbers.len()
+}
+
 pub fn distance(a: [f32;2], b: [f32;2]) -> f64 {
     (((b[0] - a[0]).powi(2) + (b[1] - a[1]).powi(2)) as f64).sqrt()
 }
